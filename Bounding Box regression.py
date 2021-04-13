08cd2de9f4eb934cf85555a86021ac64ca610938
Darth momo <80544512+Mohamed-loua@users.noreply.github.com>
2021-04-13 23:45:05 +0200

    Update Bounding Box regression.py

--- Bounding Box regression.py
+++ Bounding Box regression.py
@@ -34,3 +34,36 @@ s = "image_0001.jpg"
 image = os.path.sep.join([images_chemin,s])
 images = cv2.imread(image)
 print(load_img(image, target_size=(224,224)))
+
+# Nous avons récupéré le numéro de l'image avec les coordonnées des bounding boxs adéquates, nous voulons maintenant extraire 
+# et enregistrer uniquement les coordonnées des bounding boxs
+for x in box_real[:]:
+    x = x.split(",")
+    nom_image , startX, startY ,endX ,endY = x
+    #les coordonées des bounding box et leurs noms sont stockés des le variables que j'ai défini
+    #Maintenant nous allons normaliser les coordonnées que nous avons extraite: d'abord il faut obtenir les dimensions de l'image
+    image_path = os.path.sep.join([images_chemin,nom_image])
+    
+    #Avec open cv on récupère les dimensions de l'image
+    image = cv2.imread(image_path)
+    (h,w,d) = image.shape
+    
+    
+    #Normalisation des données 
+    startX = float(startX) / w
+    endX = float(endX) / h
+    startY = float(startY) / w
+    endY = float(endY) / h
+    
+    #Conversion de l'image en array
+    image = load_img(image_path, target_size = (224,224))
+    image = img_to_array(image)
+    data.append(image)
+    targets.append([startX,endX,startY,endY]) 
+    names.append(nom_image)
+    
+    #Test du bon fonctionnement du dictionnaire
+print(targets[0])
+# Conversion des données en array numpy et normalisation:
+data = (np.array(data))/255
+targets = np.array(targets)
